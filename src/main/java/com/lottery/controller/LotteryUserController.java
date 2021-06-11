effb3a419047fd6be6bd0296ebc7f1e84a372ce7
pranshulrohila <pranshul.ruhela@gmail.com>
2021-06-11 17:51:54 +0530

    Added login api

---
+++ src/main/java/com/lottery/controller/LotteryUserController.java
@@ -0,0 +1,34 @@
+package com.lottery.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lottery.entity.LotteryUserLoginTo;
+import com.lottery.model.LotteryUserTo;
+import com.lottery.service.LotteryUserService;
+
+@RestController
+@RequestMapping("/users/v1")
+public class LotteryUserController {
+
+	@Autowired
+	LotteryUserService lotteryUserService;
+
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@Valid @RequestBody LotteryUserLoginTo lotteryUserLoginTo) {
+		return new ResponseEntity<String>(lotteryUserService.login(lotteryUserLoginTo), HttpStatus.OK);
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<String> signUp(@Valid @RequestBody LotteryUserTo lotteryUserTo) {
+		return new ResponseEntity<String>(lotteryUserService.signUp(lotteryUserTo), HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/lottery/entity/LotteryUserLoginTo.java
@@ -0,0 +1,36 @@
+package com.lottery.entity;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class LotteryUserLoginTo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@NotBlank(message = "UserName is mandatory")
+	private String mobileNo;
+
+	@NotBlank(message = "Password is mandatory")
+	@Size(min = 5, max = 18, message = "The password '${validatedValue}' must be between {min} and {max} characters long")
+	private String password;
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
